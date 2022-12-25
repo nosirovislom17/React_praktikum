cf250ae1cc06bf5691f36b33b602e03a21922e7b
Islombek <nosirovislom07@gmail.com>
2022-12-25 23:56:20 +0500

    16-dars full application

--- src/components/Game/Item.js
+++ src/components/Game/Item.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./style.css";
 import ArrowUp from "./image/up.png";
 import ArrowDown from "./image/down.png";
 
 const Item = (props) => {
   const { votes, text } = props;
+  const getEmoji = useCallback((votes) => {
+    if (votes >= 9) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (votes >= 6) {
+      return "em em-laughing";
+    } else if (votes >= 3) {
+      return "em em-slightly_smiling_face";
+    } else if (votes >= 0) {
+      return "em em-neutral_face";
+    } else {
+      return "em em-angry";
+    }
+  }, []);
   return (
     <div className="item">
       <div className="itemBtn">
@@ -14,7 +27,7 @@ const Item = (props) => {
       </div>
       <div className="itemText">{text}</div>
       <div className="itemEmoji">
-        <i className="em em-laughing "></i>
+        <i className={getEmoji(votes)}></i>
       </div>
     </div>
   );
